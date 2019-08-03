5a0eadaac8c469bbab597ae21e792e85d5b267b2
Raj01gd <53119201+Raj01gd@users.noreply.github.com>
2019-08-03 21:21:32 +0530

    Home Page Changes

---
+++ MyFirstProject/Controllers/MyInfoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstProject.Controllers
+{
+    public class MyInfoController : Controller
+    {
+        // GET: MyInfo
+        public ActionResult Interests()
+        {
+            if (User.IsInRole("Admin"))
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect("~/Home/Contact");
+            }
+        }
+        public ActionResult FavTvSeries()
+        {
+            if (User.IsInRole("Admin"))
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect("~/Home/Contact");
+            }
+        }
+        public ActionResult FavGadjets()
+        {
+            if (User.IsInRole("Admin"))
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect("~/Home/Contact");
+            }
+        }
+    }
+}
