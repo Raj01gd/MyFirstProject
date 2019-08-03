627cbeb515effde3aa99ca618720b4440383cd02
Raj01gd <53119201+Raj01gd@users.noreply.github.com>
2019-08-03 20:24:23 +0530

    Changes

---
+++ MyFirstProject/Scripts/MyProjectScripts.js
@@ -0,0 +1,15 @@
+﻿function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".sidenav").hasClass("nav-side");
+        if (_opened === true && !clickover.hasClass("nav-side")) {
+            document.getElementById("mySidenav").style.width = "0";
+        }
+    });
+});
